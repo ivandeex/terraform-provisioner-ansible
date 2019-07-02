2b6d2aeca10178832a874de7b40363fe09716f15
Ivan Andreev <ivandeex@gmail.com>
2019-07-03 00:43:11 +0300

    investigate validaion input

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/radekg/terraform-provisioner-ansible/dbg"
 )
 
 func main() {
+	dbg.StartLog()
+	defer dbg.StopLog()
 	plugin.Serve(&plugin.ServeOpts{
 		ProvisionerFunc: func() terraform.ResourceProvisioner {
 			return Provisioner()
